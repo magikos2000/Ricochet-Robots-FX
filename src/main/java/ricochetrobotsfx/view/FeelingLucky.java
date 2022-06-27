25efc9c8a226bf672b0d57244627d9ead02185ed
Yuchen Ye <macilmeletya19@gmail.com>
2022-06-28 02:48:18 +1000

    regular update

--- src/main/java/ricochetrobotsfx/view/FeelingLucky.java
+++ src/main/java/ricochetrobotsfx/view/FeelingLucky.java
@@ -2,10 +2,21 @@ package ricochetrobotsfx.view;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import ricochetrobotsfx.model.Model;
 
 public class FeelingLucky {
+    Model model;
+
     private final BorderPane pane = new BorderPane();
     private final Scene scene = new Scene(pane, 640, 480);
+
+    public FeelingLucky() {
+        
+    }
+
+    public void updateBoard() {
+
+    }
 
     public Scene getScene() {
         return scene;
